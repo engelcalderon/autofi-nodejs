4d1dc9e28bbc00cb0a99f4252d4fac78dfa36552
Frederick Calderon <engel199702@hotmail.com>
2021-01-11 17:41:53 -0600

    Provider model created

---
+++ src/models/providers.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
+const Schema = mongoose.Schema;
+
+const ProviderSchema = new Schema({
+    uuid: {
+        type: String,
+        unique: true,
+        default: uuidv4()
+    },
+    vin: {
+        type: String,
+        default: ''
+    },
+    make: {
+        type: String,
+        default: ''
+    },
+    model: {
+        type: String,
+        default: ''
+    },
+    mileage: {
+        type: String,
+        default: ''
+    },
+    year: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: String,
+        default: ''
+    },
+    zipCode: {
+        type: String,
+        default: ''
+    },
+    createDate: {
+        type: String,
+        default: ''
+    },
+    updateDate: {
+        type: String,
+        default: ''
+    }
+});
+
+const ProviderModel = mongoose.model('provider', ProviderSchema);
+
+module.exports = ProviderModel;
